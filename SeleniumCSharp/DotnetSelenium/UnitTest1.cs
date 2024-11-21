85e1b6aee1a5c9a44343d96be9fc65f2df7738f5
Arif Bangash <arif.bangash@live.com>
2024-11-21 12:13:44 +0100

    Initial commit

---
+++ SeleniumCSharp/DotnetSelenium/UnitTest1.cs
@@ -0,0 +1,34 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace DotnetSelenium
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+            var options = new ChromeOptions();
+
+            options.AddUserProfilePreference("profile.default_content_setting_values.cookies", 2);
+            options.AddUserProfilePreference("profile.cookie_controls_mode", 1);
+            options.AddExcludedArgument("enable-automation");
+            options.AddAdditionalOption("useAutomationExtension", false);
+
+            IWebDriver _driver = new ChromeDriver(options);
+            _driver.Manage().Window.Maximize();
+            _driver.Navigate().GoToUrl("https://www.google.com");
+
+            IWebElement _element = _driver.FindElement(By.Name("q"));
+            _element.SendKeys("Selenium");
+            _element.SendKeys(Keys.Enter);
+            //_driver.Close();
+        }
+    }
+}
