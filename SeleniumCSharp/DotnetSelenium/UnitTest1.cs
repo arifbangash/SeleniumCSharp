76d2987967fec9ad539193ff92b68c109f684fc2
Arif Bangash <arif.bangash@live.com>
2024-11-21 14:21:40 +0100

    Add new test case to click on login button

--- SeleniumCSharp/DotnetSelenium/UnitTest1.cs
+++ SeleniumCSharp/DotnetSelenium/UnitTest1.cs
@@ -30,5 +30,25 @@ namespace DotnetSelenium
             _element.SendKeys(Keys.Enter);
             _driver.Close();
         }
+
+        [Test]
+        public void EAWebsiteTest()
+        {
+            var options = new ChromeOptions();
+
+            options.AddUserProfilePreference("profile.default_content_setting_values.cookies", 2);
+            options.AddUserProfilePreference("profile.cookie_controls_mode", 1);
+            options.AddExcludedArgument("enable-automation");
+            options.AddAdditionalOption("useAutomationExtension", false);
+
+            IWebDriver _driver = new ChromeDriver(options);
+            _driver.Manage().Window.Maximize();
+
+            _driver.Navigate().GoToUrl("http://eaapp.somee.com/");
+
+            var loginLink = _driver.FindElement(By.LinkText("Login"));
+
+            loginLink.Click();
+        }
     }
 }
--- SeleniumCSharp/DotnetSelenium/obj/Debug/net8.0/DotnetSelenium.AssemblyInfo.cs
+++ SeleniumCSharp/DotnetSelenium/obj/Debug/net8.0/DotnetSelenium.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetSelenium")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f9e1c6f4f1f80c13cc859a1ab9f1191ad365fa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetSelenium")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetSelenium")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
