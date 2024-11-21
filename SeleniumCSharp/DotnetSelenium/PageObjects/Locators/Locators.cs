396c1cd9e3dabd5d46dfe130c01420c86fd1e114
Arif Bangash <arif.bangash@live.com>
2024-11-21 16:12:15 +0100

    ignore

--- SeleniumCSharp/DotnetSelenium/PageObjects/Locators/Locators.cs
+++ SeleniumCSharp/DotnetSelenium/PageObjects/Locators/Locators.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotnetSelenium.PageObjects.Locators
+﻿namespace DotnetSelenium.PageObjects.Locators
 {
-    internal class Locators
+    public class Locators
     {
     }
 }
